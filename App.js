c7604ce078a25f8e35aa96a9b33b1d56f9c1633f
Dong Yuwei <newdongyuwei@gmail.com>
2019-05-09 18:03:29 +0800

    tiny english dict

---
+++ App.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  FlatList
+} from "react-native";
+
+import getSuggestions from "./trie-service.js";
+
+type Props = {};
+export default class App extends Component<Props> {
+  state = {
+    suggestions: []
+  };
+  onChangeText = text => {
+    const suggestions = getSuggestions(text.toLowerCase());
+    console.log("suggestions", suggestions);
+
+    this.setState({
+      suggestions: suggestions
+    });
+  };
+
+  keyExtractor = item => item.word;
+
+  render() {
+    console.log("render");
+    const suggestions = this.state.suggestions;
+    return (
+      <View style={styles.container}>
+        <Text>Input the word...</Text>
+        <TextInput style={styles.input} onChangeText={this.onChangeText} />
+        <FlatList
+          style={styles.list}
+          data={suggestions}
+          keyExtractor={this.keyExtractor}
+          renderItem={({ item }) => (
+            <View style={styles.item}>
+              <Text>
+                {item.word}: [ {item.ipa}] {item.translation.join(" ")}
+              </Text>
+            </View>
+          )}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+    marginTop: 30
+  },
+  input: {
+    width: "80%",
+    borderWidth: 1,
+    borderColor: "#eee",
+    borderRadius: 5,
+    margin: 10
+  },
+  list: {
+    marginTop: 5,
+    marginRight: 5,
+    marginBottom: 30,
+    marginLeft: 5
+  },
+  item: {
+    borderBottomWidth: 1,
+    borderColor: "#ddd",
+    paddingBottom: 3,
+    marginBottom: 5
+  }
+});
---
+++ trie-service.js
@@ -0,0 +1,26 @@
+const PTrie = require("dawg-lookup/lib/ptrie").PTrie;
+const words = require("./words_with_frequency_and_translation_and_ipa.json");
+
+const packed = require("./packed.json").content;
+
+const ptrie = new PTrie(packed);
+
+const getSuggestions = prefix => {
+  return ptrie
+    .completions(prefix)
+    .sort((a, b) => {
+      return (
+        (words[b] || { frequency: 0 }).frequency -
+        (words[a] || { frequency: 0 }).frequency
+      );
+    })
+    .slice(0, 30)
+    .map(word => {
+      return {
+        ...words[word],
+        word
+      };
+    });
+};
+
+export default getSuggestions;
