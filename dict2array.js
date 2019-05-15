5bda434f3a2879654ac043a1e9636ebcea91a9c2
Dong Yuwei <newdongyuwei@gmail.com>
2019-05-15 17:29:07 +0800

    change structure

--- dict2array.js
+++ dict2array.js
@@ -1,13 +1,15 @@
 const fs = require("fs");
 const dict = require("./words_with_frequency_and_translation_and_ipa.json");
 
-const dict2 = { keys: [], values: [] };
+const dict2 = { entries: [] };
 
 Object.keys(dict)
   .sort()
   .forEach(key => {
-    dict2.keys.push(key);
-    dict2.values.push(dict[key]);
+    dict2.entries.push({
+      key: key,
+      value: dict[key]
+    });
   });
 
 fs.writeFileSync(
@@ -15,4 +17,4 @@ fs.writeFileSync(
   JSON.stringify(dict2),
   "utf-8"
 );
-console.log(dict2.keys.length);
+console.log(dict2.entries.length);
