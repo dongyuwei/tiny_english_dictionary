be8cc65c1c1bea6d853422bc15d9135cca88602d
Dong Yuwei <newdongyuwei@gmail.com>
2019-05-20 14:58:27 +0800

    Revert "[WIP] use nspell todo spell check. works good on ios, crash on android"
    This reverts commit 2cc30e776b00392172103350b3b97805f35d871f.

--- trie-service.js
+++ trie-service.js
@@ -1,12 +1,12 @@
 const PTrie = require("dawg-lookup/lib/ptrie").PTrie;
 const words = require("./words_with_frequency_and_translation_and_ipa.json");
+
 const packed = require("./packed.json").content;
-const spellchecker = require("./spell-checker.js");
 
 const ptrie = new PTrie(packed);
 
 const getSuggestions = prefix => {
-  let suggestion = ptrie
+  return ptrie
     .completions(prefix)
     .sort((a, b) => {
       return (
@@ -21,19 +21,6 @@ const getSuggestions = prefix => {
         word
       };
     });
-
-  if (suggestion.length === 0) {
-    suggestion = spellchecker.suggest(prefix);
-    if (suggestion.length > 0) {
-      suggestion = suggestion.map(item => {
-        return {
-          ...words[item],
-          word: item.toLowerCase()
-        };
-      });
-    }
-  }
-  return suggestion;
 };
 
 module.exports = getSuggestions;
