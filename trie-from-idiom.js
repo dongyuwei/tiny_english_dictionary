841a52a2c81a69e5497052067f6e0b456fdcd6bc
Dong Yuwei <newdongyuwei@gmail.com>
2019-10-06 23:23:33 +0800

    cleanup code

--- trie-from-idiom.js
+++ trie-from-idiom.js
@@ -1,12 +1,9 @@
 const Trie = require('node-ternary-search-trie');
-// const trie = new Trie();
-// const Trie = require('tiny-trie-js');
 const trie = new Trie();
 const data = require('./idiom.json');
 data.forEach(element => {
   trie.set(element, 0);
 });
-// console.log(222, trie.keysWithPrefix('川'));
 module.exports = function keysWithPrefix(prefix) {
   return trie.keysWithPrefix(prefix);
 };
